0492f73762577df36c4bdce187e54e4c94ca727a
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-01-02 21:33:22 +0200

    3d.py

--- 3d.py
+++ 3d.py
@@ -1,18 +1,21 @@
 #pip install panda3d
-# from unittest import loader
 
 from direct.showbase.ShowBase import ShowBase #сцена
-base = ShowBase() #об'єкт від класу
-base.run()
+#base = ShowBase() 
+# ↑ об'єкт від класу
+#base.run() 
+# ↑ викл. метод run
 
-#сцена, висота персонажа,
+#сцена, висота персонажа..
 class Game(ShowBase):
     def __init__(self):
-        self.model = loader.loadModel('models/enviroment')
+        ShowBase.__init__(self)
+        #self.model = loader.loadModel('models/enviroment')
+        self.model = loader.loadModel('models/Fighter.egg')
         self.model.reparentTo(render)
-        self.model.setScale(0.1)self.model
+        self.model.setScale(0.1)#self.model
         self.model.setPos(-2, 25, -3)
-        base.camlens.setFov(90) #189 max
+        base.camLens.setFov(90) #189 max
 
 game = Game()
 game.run()
