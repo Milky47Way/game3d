967c9666cb5b3cd1a0b5e4f38b93e4e4e4df8b6a
Milk <145270243+Milky47Way@users.noreply.github.com>
2025-01-04 13:16:35 +0200

    Add files via upload

--- hero.py
+++ hero.py
@@ -10,13 +10,17 @@ key_down = 'v' #вниз
 
 key_turn_left = 'q'
 key_turn_right = 'e'
-step = 0.2
+
+key_build = 'mouse1'
+key_destroy = 'mouse3'
+
+step = 1
 
 class Hero():
     def __init__(self, pos, land):
         self.land = land
         self.mode = True #крізь усе
-        self.hero = loader.loadMode('smiley') #hero
+        self.hero = loader.loadMode('smiley') #hero.egg + texture
         self.hero.setColor(1, 0.5, 0)
         self.hero.setColor(0.3)
         self.hero.setPos(pos)
@@ -58,7 +62,7 @@ class Hero():
         y_to = y_from + dy
         return x_to, y_to, z_from
 
-    def just_move(selfself, angle):
+    def just_move(self, angle):
         x, y, z = self.look_at(angle)
         if not self.land.isBlockAt((round(x), round(y), round(z))):
             self.hero.setPos((x, y, z))
@@ -71,19 +75,19 @@ class Hero():
 
         x_from = self.hero.getX()
         y_from = self.hero.getY()
-        z_from= self.hero.getz()
-        dx, dy self.check_dir(angle)
+        z_from = self.hero.getz()
+        dx, dy = self.check_dir(angle)
         x_to = x_from + dx
         y_to = y_from + dy
         return x_to, y_to, z_from
 
     def just_move(self, angle):
-    x, y, z self.look_at(angle)
-    if not self.land.isBlockAt((round(x), round(y), round(z))):
-        self.hero.setPos((x, y, z))
-        print(f"Герой переміщений на {(x, y, z)}")
-    else:
-        print(f"Неможливо рухатись, є блок на {(x, y, z)}")
+        x, y, z = self.look_at(angle)
+        if not self.land.isBlockAt((round(x), round(y), round(z))):
+            self.hero.setPos((x, y, z))
+            print(f"Герой переміщений на {(x, y, z)}")
+        else:
+            print(f"Неможливо рухатись, є блок на {(x, y, z)}")
 
     def move_to(self, angle):
         if self.mode:
@@ -91,63 +95,63 @@ class Hero():
 
 
     def check_dir(self, angle):
-    if angle >= 0 and angle <= 20:
-        return (0, -step)
-    elif angle <= 65:
-        return (step, -step)
-    elif angle <= 110:
-        return (step, 0)
-    elif angle <= 155:
-        return (step, step)
-    elif angle <= 200:
-        return (0, step)
-    elif angle <= 245:
-        return (-step, step)
-    elif angle <= 290:
-        return (-step, 0)
-    elif angle <= 335:
-        return (-step, -step)
-    else:
-        return (0, -step)
+        if angle >= 0 and angle <= 20:
+            return (0, -step)
+        elif angle <= 65:
+            return (step, -step)
+        elif angle <= 110:
+            return (step, 0)
+        elif angle <= 155:
+            return (step, step)
+        elif angle <= 200:
+            return (0, step)
+        elif angle <= 245:
+            return (-step, step)
+        elif angle <= 290:
+            return (-step, 0)
+        elif angle <= 335:
+            return (-step, -step)
+        else:
+            return (0, -step)
 
     def forward(self):
-        angle (self.hero.getH()) % 360
+        angle = (self.hero.getH()) % 360
         self.move_to(angle)
 
     def back(self):
-        angle (self.hero.getH() + 180) % 360
+        angle = (self.hero.getH() + 180) % 360
         self.move_to(angle)
         
     def left(self):
-        angle (self.hero.getH() + 90) % 360
+        angle = (self.hero.getH() + 90) % 360
         self.move_to(angle)
         
     def right(self):
-        angle (self.hero.getH() + 270) % 360
+        angle = (self.hero.getH() + 270) % 360
         self.move_to(angle)
 
     def move_up(self):
-        x, y, z self.hero.getPos()
-        if not z 5:
+        x, y, z = self.hero.getPos()
+        if not z == 5:
             new_pos = (x, y, z + step)
             self.hero.setPos(new_pos)
 
     def move_down(self):
         x, y, z = self.hero.getPos()
-        if not z == 0 and not self.land.isBlockAt((round(x), round(y), round(z step))):
-        new_pos (x, y, z - step)
-        self.hero.setPos(new_pos)
+        if not z == 0 and not self.land.isBlockAt((round(x), round(y), round(z - step))):
+            new_pos = (x, y, z - step)
+            self.hero.setPos(new_pos)
 
     def accept_events (self):
         base.accept(key_turn_left, self.turn_left)
-        base.accept(key_turn_left-repeat', self.turn_left)
+        base.accept(key_turn_left + '-repeat', self.turn_left)
         base.accept(key_turn_right, self.turn_right)
         base.accept(key_turn_right + '-repeat', self.turn_right)
                     
         base.accept(key_forward, self.forward)
         base.accept(key_forward + '-repeat', self.forward)
         base.accept(key_back, self.back)
-        base.accept(key_back +-repeat', self.back)
+        base.accept(key_back + '-repeat', self.back)
         base.accept(key_left, self.left)
         base.accept(key_left + '-repeat', self.left)
         base.accept(key_right, self.right)
